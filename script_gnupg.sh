98d7b1b55a20e87ff1fc02f7ac19cedb9629371b
itsAndy35 <anguamingaul@uide.edu.ec>
2026-03-25 02:31:23 +0000

    signed document

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -5,3 +5,4 @@ gpg --import bm_llave_publica.asc #importar la llave de mi compa
 echo "mensaje" > doc_no_cifrado.txt #creamos el documento con el mensaje no cifrado
 gpg --output doc_cifrado.txt --encrypt --recipient XXXXXXX doc_no_cifrado.txt #We use the hash to encrypt the message
 gpg --decrypt bm_doc_cifrado.txt #decipher my partner's document
+gpg --output doc_no_cifrado_firmado.txt --clearsign doc_no_cifrado.txt #documento firmado
