e5ee1719fb5a49211bf38b224f361961ee6498f6
itsAndy35 <anguamingaul@uide.edu.ec>
2026-03-25 23:56:32 +0000

    commit integrante 2

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -1,17 +1,16 @@
-gpg --full-generate-key #generar llave publica
+gpg --full-generate-key #generate public key
 gpg --armor --export #Export the public key in a human-readable format
-gpg --list-keys #lista de llaves
-gpg --import bm_llave_publica.asc #importar la llave de mi compa
-echo "mensaje" > doc_no_cifrado.txt #creamos el documento con el mensaje no cifrado
+gpg --list-keys #displays the list of keys
+gpg --import bm_llave_publica.asc #My partner's key matters
+echo "mensaje" > doc_no_cifrado.txt #We created the document with the unencrypted message.
 gpg --output doc_cifrado.txt --encrypt --recipient XXXXXXX doc_no_cifrado.txt #We use the hash to encrypt the message
 gpg --decrypt bm_doc_cifrado.txt #decipher my partner's document
-gpg --output doc_no_cifrado_firmado.txt --clearsign doc_no_cifrado.txt #documento firmado
+gpg --output doc_no_cifrado_firmado.txt --clearsign doc_no_cifrado.txt #We signed the unencrypted document.
 gpg --verify bm_doc_no_cifrado_firmado.txt #We verified the signature
-gpg --edit-key B71FA457F258D #editar la llave de mi compa;ero
-gpg --verify bm_doc_no_cifrado_firmado.txt #verificamos el nuevo nivel de confianza al documento
-gpg --output doc_no_firmado_binario.txt --sign doc_no_cifrado.txt #firma de documento en binario
-gpg --verify bm_doc_no_cifrado_firmado_binario.txt #verificamos la firma en binario
-gpg --output firma_separada_doc_no_cifrado.sig --detach-sign doc_no_cifrado.txt #separamos la firma del documento
-gpg --verify bm_firma_separada_doc_no_cifrado.sig bm_doc_no_cifrado.txt #separamos la firma separada del documento de neustro compa;ero
-gpg --output doc_cifrado_y_firmado.txt --encrypt --sign --recipient A021DAD50ABBF516 doc_no_cifrado.txt #firmar y cifrar un documento
-gpg --output doc_cifrado_y_firmado_descifrado_y_validado.txt --decrypt bm_doc_cifrado_y_firmado.txt #validar firma y descifrar
+gpg --edit-key B71FA457F258D #edit my colleague's key
+gpg --verify bm_doc_no_cifrado_firmado.txt #We verified the new level of confidence in the document
+gpg --output doc_no_firmado_binario.txt --sign doc_no_cifrado.txt #binary document signature
+gpg --verify bm_doc_no_cifrado_firmado_binario.txt #verify the signature in binary
+gpg --output firma_separada_doc_no_cifrado.sig --detach-sign doc_no_cifrado.txt #We separated the signature from the document.
+gpg --verify bm_firma_separada_doc_no_cifrado.sig bm_doc_no_cifrado.txt #We separated the signature from our colleague's document.
+gpg --output doc_cifrado_y_firmado.txt --encrypt --sign --recipient A021DAD50ABBF516 doc_no_cifrado.txt #sign and encrypt a document
