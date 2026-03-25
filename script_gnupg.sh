141c4187e53064fa720664ef398980332eecdd88
itsAndy35 <anguamingaul@uide.edu.ec>
2026-03-25 02:04:11 +0000

    cat message to verify

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -1,3 +1,5 @@
 gpg --full-generate-key #generar llave publica
 gpg --armor --export #Export the public key in a human-readable format
 gpg --list-keys #lista de llaves
+gpg --import bm_llave_publica.asc #importar la llave de mi compa
+echo "mensaje" > doc_no_cifrado.txt #creamos el documento con el mensaje no cifrado
