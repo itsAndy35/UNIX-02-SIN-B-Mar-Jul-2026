dd37021651d74a1a7d0309351fc06ed859aa2ecc
itsAndy35 <anguamingaul@uide.edu.ec>
2026-03-25 13:46:46 +0000

    It displays detailed information and metadata about a file.

--- filesystem.sh
+++ filesystem.sh
@@ -2,3 +2,5 @@ pwd #print working directory
 cd #change directory
 ls -F #List the contents of the current working directory
 ls -i #list files
+echo "mensaje" > test.txt #Create a file with the specific text
+stat test.txt #It displays detailed information and metadata about a file.
