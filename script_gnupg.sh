d3b88bc94ea2de81e517a73a8e367de54f6b01fa
itsAndy35 <anguamingaul@uide.edu.ec>
2026-03-25 02:12:14 +0000

    We use the hash to encrypt the message

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -3,3 +3,4 @@ gpg --armor --export #Export the public key in a human-readable format
 gpg --list-keys #lista de llaves
 gpg --import bm_llave_publica.asc #importar la llave de mi compa
 echo "mensaje" > doc_no_cifrado.txt #creamos el documento con el mensaje no cifrado
+gpg --output doc_cifrado.txt --encrypt --recipient XXXXXXX doc_no_cifrado.txt #We use the hash to encrypt the message
