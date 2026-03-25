e4692ae04107be6b256170fd4563462658bee918
itsAndy35 <anguamingaul@uide.edu.ec>
2026-03-25 02:27:06 +0000

    decipher my partner's document

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -4,3 +4,4 @@ gpg --list-keys #lista de llaves
 gpg --import bm_llave_publica.asc #importar la llave de mi compa
 echo "mensaje" > doc_no_cifrado.txt #creamos el documento con el mensaje no cifrado
 gpg --output doc_cifrado.txt --encrypt --recipient XXXXXXX doc_no_cifrado.txt #We use the hash to encrypt the message
+gpg --decrypt bm_doc_cifrado.txt #decipher my partner's document
