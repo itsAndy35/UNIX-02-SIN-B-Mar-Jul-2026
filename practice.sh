37bdffa032ce7d428dc4451ed2d6cffd923ba01d
itsAndy35 <anguamingaul@uide.edu.ec>
2026-03-30 12:56:46 +0000

    updates all software packages installed

--- practice.sh
+++ practice.sh
@@ -1,2 +1,2 @@
 sudo apt update #update local package index from configured repositories on linux
-sudo apt upgrade
+sudo apt upgrade #updates all software packages installed on debian based linux system to their lastest versions
