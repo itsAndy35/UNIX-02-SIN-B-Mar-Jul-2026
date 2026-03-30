50295af64fa3bbd54fb4b495368865ff3eaf829d
itsAndy35 <anguamingaul@uide.edu.ec>
2026-03-30 13:04:05 +0000

    create, delete, resize, move and copy disk partitions

--- practice.sh
+++ practice.sh
@@ -1,2 +1,3 @@
 sudo apt update #update local package index from configured repositories on linux
 sudo apt upgrade #updates all software packages installed on debian based linux system to their lastest versions
+sudo apt install parted #comannd line designed to create, delete, resize, move and copy disk partitions
