d14aec81ecaa0ae38133db4a2ccf4ee2bb44b9fa
itsAndy35 <anguamingaul@uide.edu.ec>
2026-03-30 13:45:47 +0000

    display detailed information and metadata of a file or file system

--- practice.sh
+++ practice.sh
@@ -4,3 +4,4 @@ sudo apt install parted #comannd line designed to create, delete, resize, move a
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n" #command to get a detailed view of disk partitions
 [ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS" #This command generates a condition to confirm in which mode my system starts.
 echo "esto es un archivo" > archivo.txt #Create a file with the specific text
+stat archivo.txt #display detailed information and metadata of a file or file system
