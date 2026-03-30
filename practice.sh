8ee464ec5323c56c6033f47a5bd8567e3223f6d7
itsAndy35 <anguamingaul@uide.edu.ec>
2026-03-30 13:12:32 +0000

    My system has booted into BIOS mode

--- practice.sh
+++ practice.sh
@@ -2,3 +2,5 @@ sudo apt update #update local package index from configured repositories on linu
 sudo apt upgrade #updates all software packages installed on debian based linux system to their lastest versions
 sudo apt install parted #comannd line designed to create, delete, resize, move and copy disk partitions
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n" #command to get a detailed view of disk partitions
+[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS"
+BIOS #This command generates a condition to confirm in which mode my system starts.
