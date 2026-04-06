5456d86213363a82abd96b1fce4f855a29c5fddd
itsAndy35 <anguamingaul@uide.edu.ec>
2026-04-06 12:44:24 +0000

    in-depth technical inspection of a folder.

--- script_boot.sh
+++ script_boot.sh
@@ -2,3 +2,9 @@ cd #Terminal tool to change current working directory
 cd /home/codespace #change directory 
 cd ~ # special character that Linux automatically interprets as "the current user's home".
 cd $HOME #Save the path to your home folder in a variable called HOME
+mkdir proyecto # Create a new folder called "project" in your current location.
+cd proyecto/ #change directory to "proyecto/"
+ls -lai #in-depth technical inspection of a folder.
+total 12 # Total disk space in blocks used by this directory
+925542 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:36 .
+918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:36 ..
