1ab86c416c15f193c4dd62df40d217b2589a5d4e
itsAndy35 <anguamingaul@uide.edu.ec>
2026-04-06 12:35:37 +0000

    Save the path to your home folder

---
+++ script_boot.sh
@@ -0,0 +1,4 @@
+cd #Terminal tool to change current working directory
+cd /home/codespace #change directory 
+cd ~ # special character that Linux automatically interprets as "the current user's home".
+cd $HOME #Save the path to your home folder in a variable called HOME
