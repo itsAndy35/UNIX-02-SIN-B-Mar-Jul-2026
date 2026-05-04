d4816f1eb7818ec5ce71d7e0808eb1235a40cfbe
itsAndy35 <anguamingaul@uide.edu.ec>
2026-05-04 01:19:35 +0000

    change directory

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -6,3 +6,6 @@ ls -r #List contents in reverse alphabetical order
 ls -rl #List in long format and in reverse order
 aptitude -v moo #Insists on seeing the Easter egg; the system responds that there isn't one
 pwd #Print the full path of the current working directory
+cd #change directory
+cd Documents #Change the current directory to the "Documents" folder
+cd / #Change to the system root directory
