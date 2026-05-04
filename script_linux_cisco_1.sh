f130508f245afa9e502042f10f57ef538d08c550
itsAndy35 <anguamingaul@uide.edu.ec>
2026-05-04 01:06:22 +0000

    insisit esater egg

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -4,3 +4,4 @@ aptitude moo #displays a humorous message denying the existence of an Easter egg
 ls -l #List files in long format with technical details
 ls -r #List contents in reverse alphabetical order
 ls -rl #List in long format and in reverse order
+aptitude -v moo #Insists on seeing the Easter egg; the system responds that there isn't one
