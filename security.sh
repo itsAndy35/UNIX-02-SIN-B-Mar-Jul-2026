85ce7069852678459ade45476d5ec82b5e93242a
itsAndy35 <anguamingaul@uide.edu.ec>
2026-05-04 13:42:06 +0000

    root user

--- security.sh
+++ security.sh
@@ -3,3 +3,6 @@ cat /etc/passwd | head -10 #Reads the password file and shows the first 10 lines
 cat /etc/group | head -10 #Reads the group file and shows the first 10 lines 
 groups # Shows the names of all groups for the current user
 groups $USER #Shows groups for the specific user stored in the variable
+cat /etc/group | grep games #Finds the 'games' group entry in the groups file
+mkdir ~/proyecto_unix/ #Creates a directory named 'proyecto_unix' in the home folder
+ls -la ~/proyecto_unix/ #Lists all files in the directory with detailed info
