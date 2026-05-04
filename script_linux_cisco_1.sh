cef6dc67f8e0f8481bc84332666549f1097504f6
itsAndy35 <anguamingaul@uide.edu.ec>
2026-05-04 01:04:17 +0000

    ls long and inverter

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -3,3 +3,4 @@ ls Documents #List the contents of the "Documents" folder
 aptitude moo #displays a humorous message denying the existence of an Easter egg
 ls -l #List files in long format with technical details
 ls -r #List contents in reverse alphabetical order
+ls -rl #List in long format and in reverse order
