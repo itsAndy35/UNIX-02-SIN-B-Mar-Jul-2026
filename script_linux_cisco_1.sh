daf8f566f05dc2f8f061cb8dca761ed3f6cffe34
itsAndy35 <anguamingaul@uide.edu.ec>
2026-05-04 01:00:40 +0000

    ls inverse

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -2,3 +2,4 @@ ls #list files
 ls Documents #List the contents of the "Documents" folder
 aptitude moo #displays a humorous message denying the existence of an Easter egg
 ls -l #List files in long format with technical details
+ls -r #List contents in reverse alphabetical order
