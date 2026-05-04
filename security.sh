6e2db4759d58169c0a00eb38ea7b8e5b7218986c
itsAndy35 <anguamingaul@uide.edu.ec>
2026-05-04 13:30:31 +0000

    groups

--- security.sh
+++ security.sh
@@ -1,4 +1,5 @@
 id #Displays the user and group IDs
 cat /etc/passwd | head -10 #Reads the password file and shows the first 10 lines
+cat /etc/group | head -10 #Reads the group file and shows the first 10 lines 
 groups # Shows the names of all groups for the current user
 groups $USER #Shows groups for the specific user stored in the variable
