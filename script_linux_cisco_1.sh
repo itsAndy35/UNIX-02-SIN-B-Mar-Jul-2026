c04ebd74dee5444695f329f985cc832a146acf58
itsAndy35 <anguamingaul@uide.edu.ec>
2026-05-04 01:13:05 +0000

    print the directory

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -5,3 +5,4 @@ ls -l #List files in long format with technical details
 ls -r #List contents in reverse alphabetical order
 ls -rl #List in long format and in reverse order
 aptitude -v moo #Insists on seeing the Easter egg; the system responds that there isn't one
+pwd #Print the full path of the current working directory
