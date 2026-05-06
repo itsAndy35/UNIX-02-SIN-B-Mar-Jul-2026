a0a8de4ff72c51baf726dec6b78d677a2bc103a6
itsAndy35 <anguamingaul@uide.edu.ec>
2026-05-06 12:38:29 +0000

    grep

--- security.sh
+++ security.sh
@@ -6,3 +6,8 @@ groups $USER #Shows groups for the specific user stored in the variable
 cat /etc/group | grep games #Finds the 'games' group entry in the groups file
 mkdir ~/proyecto_unix/ #Creates a directory named 'proyecto_unix' in the home folder
 ls -la ~/proyecto_unix/ #Lists all files in the directory with detailed info
+sudo groupadd desarrolladores
+sudo gorupadd -g 2000 operaciones
+sudo groupadd --system servicios_web
+grep "desarrolladores\|operaciones\|servicio_web" /etc/group
+grep -E "desarrolladores|operaciones|servicios_web" /etc/group
