e8fa7a7a2deb327259edf4146a50098c48dbf4f8
itsAndy35 <anguamingaul@uide.edu.ec>
2026-06-01 13:38:03 +0000

    sh3

---
+++ blackhatbash2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# All this script does is create a directory, create a file
+set -x
+mkdir mydirectory
+touch mydirectory/myfile
+ls -l mydirectory
+set +x
+#+ set -x
+#+ mkdir mydirectory
+#mkdir: cannot create directory ‘mydirectory’: El fichero ya existe
+#+ touch mydirectory/myfile
+#+ ls -l mydirectory
+#total 0
+#-rw-rw-rw- 1 root root 0 jun  1 13:27 myfile
++ set +x
