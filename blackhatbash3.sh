07b4be80e7b0bed65f21360a9f37f8d93b78f399
itsAndy35 <anguamingaul@uide.edu.ec>
2026-06-01 13:46:55 +0000

    unset

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -7,3 +7,16 @@ echo "This book's name is ${book}"
 #This book's name is black hat bash
 root_directory=$(ls -ld /)
 echo "${root_directory}"
+book="Black Hat Bash"
+unset book
+echo "${book}"
+#+ book='black hat bash'
+#+ echo 'This book'\''s name is black hat bash'
+#This book's name is black hat bash
+#++ ls -ld /
+#+ root_directory='drwxr-xr-x 1 root root 4096 jun  1 12:38 /'
+#+ echo 'drwxr-xr-x 1 root root 4096 jun  1 12:38 /'
+#drwxr-xr-x 1 root root 4096 jun  1 12:38 /
+#+ book='Black Hat Bash'
+#+ unset book
+#+ echo ''
