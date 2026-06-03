bd0eccb21cf121ee653bf4331a190aa698f349cf
itsAndy35 <anguamingaul@uide.edu.ec>
2026-06-03 13:35:01 +0000

    error

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+set -x
 touch test && touch test123
 (ls; ps)
 ls; ps; whoami
 lzl || echo "el comando lzl falló"
+echo "uno" > archivo.txt
+cat archivo.txt
+echo "uno-uno" > archivo.txt
+cat archivo.txt
+echo "dos-dos" > archivo.txt
+cat archivo.txt
+echo "tres" &> archivo.txt
+cat archivo.txt
+echo "tres" &>> archivo.txt
+cat archivo.txt
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt 
+lzl 2> error.txt
+cat error.txt
+
+set +x
