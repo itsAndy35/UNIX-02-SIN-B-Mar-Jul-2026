35846e819a4136c660753c5aeea11ae71e4252fd
itsAndy35 <anguamingaul@uide.edu.ec>
2026-06-03 12:48:12 +0000

    delete

--- array.sh
+++ array.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 #set an array
 IP_ADDRESSES=(192.168.1.1 192.168.1.2 192.168.1.3)
 
@@ -8,3 +8,7 @@ echo "${IP_ADDRESSES[*]}"
 
 # Prints only the first element in the array 
 echo "${IP_ADDRESSES[0]}"
+
+unset IP_ADDRESSES[1]
+echo "${IP_ADDRESSES[*]}"
+set +x
